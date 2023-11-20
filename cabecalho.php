362d71c397d54878a1b9274358d9b63b1a96716a
Gabriel Lucas <biel34824@gmail.com>
2023-11-19 21:49:01 -0300

    adicionado rodape e cabecalho

---
+++ cabecalho.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="cabecalho.css" rel="stylesheet">
+
+    <title>FUSB</title>
+</head>
+
+<body>
+    <div id="menu">
+        <nav class="dp-menu">
+            <ul>
+                <li>
+                    <a href="Inicio.php">
+                        PÁGINA INICIAL
+                    </a>
+                </li>
+                <li>
+                    <a href="#">PRÓ-REITORIAS</a>
+                    <ul>
+                        <li><a href="#">PROPA </a></li>
+                        <li><a href="#"> PROGEAC </a></li>
+                        <li><a href="#"> PROPPG</a></li>
+                        <li><a href="#"> PROGEPE</a></li>
+                        <li><a href="#"> PROAF</a></li>
+                        <li><a href="#"> PROEX</a></li>
+                        <li><a href="#"> PROPLAN</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">ENSINO</a>
+                    <ul>
+                        <li><a href="#">GRADUAÇÃO</a></li>
+                        <li><a href="#">PÓS-GRADUAÇÃO</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">
+                        PESQUISA
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        EXTENSÃO
+                    </a>
+
+                <li>
+                    <a href="#">
+                        SERVIÇOS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        ACESSO A INFORMAÇÃO
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        FALE CONOSCO
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        CALENDÁRIO
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        EDITAIS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        CONCURSOS
+                    </a>
+                </li>
+                <li>
+                    <a href="formIncri.php">
+                        PROCESSO SELETIVO 2024
+                    </a>
+                </li>
+
+
+
+            </ul>
+
+        </nav>
+    </div>
+
+</body>
+
+</html>
---
+++ rodape.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="rodape.css">
+
+  <title>FUSB</title>
+</head>
+
+<body class="container-fluid">
+  <div id="rodape">
+
+  <div class="conteudo">
+
+  </div>
+
+      <footer class="rodape" id="contato">
+        <div class="rodape-div">
+    
+            <div class="rodape-div-1">
+                <div class="rodape-div-1-coluna">
+       
+                    <span><b>Faculdade Unidos do Sul da Bahia </b></span>
+                    <p>Avenida Itabuna - Ilhéus - BA, 45653-160</p>
+                </div>
+            </div>
+    
+            <div class="rodape-div-2">
+                <div class="rodape-div-2-coluna">
+                 
+                    <span><b>Fale Conosco</b></span>
+                    <p>centraldeatendimento@fusb.com.br</p>
+                    <p>+55 73 99200-0000</p>
+                </div>
+            </div>
+    
+            <div class="rodape-div-3">
+                <div class="rodape-div-3-coluna">
+               
+                    <span><b>Ensino</b></span>
+                    <p><a href”#graduacao"></a>Graduação</p>
+                    <p><a href="#posgraduacao">Pós Graduação </a></p>
+                    <p><a href="#extensao">Extensão</a></p>
+                </div>
+            </div>
+    
+            <div class="rodape-div-4">
+                <div class="rodape-div-4-coluna">
+                   
+                    <span><b> A FUSB</b></span>
+                     <p><a href="#fusb">FUSB</a></p>
+                       <p><a href="#servicos">Serviços</a></p>
+                       <p><a href="#acessoainformacao">Acesso à Informação </a></p>
+                </div>
+            </div>
+    
+        </div>
+        <p class="rodape-direitos"> © 2023 – Faculdade Unidos do Sul da Bahia.</p>
+    </footer>
+</body>
+
+
+</html>
